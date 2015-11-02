738240d324cffdc26081d8f5f4b1a7ec9fd17b7f
Allison Sass <allison.sass@gmail.com>
2015-11-02 11:23:34 -0600

    minor js changes

--- apps/app.js
+++ apps/app.js
@@ -22,17 +22,21 @@ function getRequest(searchTerm){
 
   url = 'https://www.googleapis.com/youtube/v3/search';
 
+  //to find out name of array: https://www.googleapis.com/youtube/v3/search?part=snippet&q=test&key=AIzaSyCdtseV-3CvBa8VPeQLAZmVU9ddlj3ffnA
+
   $.getJSON(url, params, function(data){
     showResults(data.items);
   });
 }
 
 
-function showResults(videos){
+function showResults(name){
 	var html = "";
+
+  //Useful resource for video ID http://stackoverflow.com/questions/32410837/get-youtube-video-url-using-youtube-v3-api-javascript
   
-  $.each(videos, function(index,value){
-  	html += "<div class='video-display'><p>" + value.snippet.title + "</p><a target='_blank' href='https://www.youtube.com/watch?v=" + value.id.videoId + "'><img width='320px' height='270px'src='" +  value.snippet.thumbnails.high.url + "'/></a></div>" ;
+  $.each(name, function(index,value){
+  	html += "<div class='video-display'><p>" + value.snippet.title + "</p><a target='_blank' href='https://www.youtube.com/watch?v=" + value.id.videoId + "'><img width='320px' height='270px'src='" +  value.snippet.thumbnails.medium.url + "'/></a></div>" ;
     console.log(value.snippet.title);
   });
 
